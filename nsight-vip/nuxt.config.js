9d13fc6c18e14effeefcfe4260d501e73c29cc6d
OlivierC <coq.olivier36@gmail.com>
2022-09-01 01:29:19 -0400

    Added shop and product pages

--- nsight-vip/nuxt.config.js
+++ nsight-vip/nuxt.config.js
@@ -45,8 +45,16 @@ export default {
     ]
   },
 
+  // Axios
+  publicRuntimeConfig: {
+    axios: {
+      baseURL: 'http://localhost:1337/api'
+    }
+  },
+
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/axios'
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
---
+++ nsight-vip/pages/shop/products/_id/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div></div>
+</template>
+<script>
+    export default {
+        name: 'Product',
+        data(){
+            return {
+                product: null,
+                sotreUrl: process.env.storeUrl
+            }
+        },
+        created() {
+            this.$axios.$get(`http://localhost:1337/api/products/${this.$route.params.id}?populate=*`)
+                .then((data) => { this.product = data })
+        }
+    }
+</script>
+<style lang="scss">
+
+</style>
