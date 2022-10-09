38a027cb24a0c26fb616f5e5b67c7f46657f0ecd
OlivierC <coq.olivier36@gmail.com>
2022-10-08 20:02:12 -0400

    Removing old project folder

--- nsight-vip/layouts/default.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div>
-        <Nuxt/>
-        <div hidden id="snipcart" data-api-key="ZjU5ZDBjNmEtYzBlYi00YjVhLWFlNzYtNDE2ZjhlMGI2MjkxNjM3OTcwNzY0NzA1MjM5NTcz"></div>
-    </div>
-</template>
