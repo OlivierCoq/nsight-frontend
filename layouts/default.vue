9b3dbe0d683ab13f39bbeab1ecc59a91cebf6ca7
OlivierC <coq.olivier36@gmail.com>
2022-10-08 21:00:55 -0400

    ci cd update

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,4 +3,4 @@
         <Nuxt/>
         <div hidden id="snipcart" data-api-key="ZjU5ZDBjNmEtYzBlYi00YjVhLWFlNzYtNDE2ZjhlMGI2MjkxNjM3OTcwNzY0NzA1MjM5NTcz"></div>
     </div>
-</template>
+</template> 
