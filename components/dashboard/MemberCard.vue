38c2ddecd3dd8927cfd0c2fa7332519f76f16f02
OlivierC <coq.olivier36@gmail.com>
2024-03-11 00:25:35 -0400

    Adding products endpoint data and updating login ui

--- components/dashboard/MemberCard.vue
+++ components/dashboard/MemberCard.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="w-[90%] h-[360px] mx-auto rounded-md shadow-md bg-zinc-200 dark:bg-zinc-400">
+  <div class="w-[90%] h-[360px] mx-auto mb-6 rounded-md shadow-md bg-zinc-200 dark:bg-zinc-400">
     <div 
       class="w-full h-1/2 rounded-t-md bg-cover bg-center"
       :style="{ backgroundImage: `url(${state.member.profile_picture ? state.member.profile_picture : 'https://res.cloudinary.com/nsight/image/upload/v1693447226/placeholder_pfp_sv2pgb.jpg'})` }"
     ></div>
     <div class="w-full h-1/2 flex flex-col justify-between items-start p-4">
-      <h3 class="text-2xl member_name text-neutral-300">{{ state.member.first_name }}</h3>
+      <h3 class="text-2xl member_name text-neutral-500 dark:text-neutral-200">{{ state.member.first_name }}</h3>
       <div class="w-full flex flex-row my-3 items-end justify-end">
-        <button @click="gift_user" class="btn-gift me-3 text-neutral-300 hover:text-blue-300">
+        <button @click="gift_user" class="btn-gift me-3 text-neutral-500 dark:text-neutral-200 hover:text-blue-300">
           <font-awesome-icon :icon="['fas', 'gift']" />
           <!-- <span class="ms-2">Send Gift</span> -->
         </button>
-        <button @click="visit_profile" class="btn-profile me-3 text-neutral-300 hover:text-green-500">
+        <button @click="visit_profile" class="btn-profile me-3 text-neutral-500 dark:text-neutral-200 hover:text-green-500">
           <font-awesome-icon :icon="['fas', 'user']" />
           <!-- <span class="ms-2">View Profile</span> -->
         </button>
--- pages/shop/products.vue
+++ pages/shop/products.vue
@@ -1,5 +1,18 @@
 <template>
-  <div></div>
+  <div class="h-[100vh] w-full bg-zinc-200 dark:bg-zinc-800 flex flex-col">
+
+      <!-- Catagories bar -->
+      <div class="w-full h-[100px] p-4 flex flex-col justify-center align-center  items-center">
+        <h1 class="text-3xl text-neutral-900 dark:text-neutral-200 uppercase font-thin">Catagory</h1>
+      </div>
+
+      <!-- Products -->
+      <div class="container mx-auto my-5">
+        <div class="w-full grid grid-cols-1 md:grid-cols-4">
+
+        </div>
+      </div>
+  </div>
 </template>
 <script>
 import { reactive, onBeforeMount } from "vue";
